803a251d5c116af13a40f552d570cf754d107bec
wss-git <wssgryx@163.com>
2021-09-02 15:04:29 +0800

    Short version ncc

--- src/src/index.ts
+++ src/src/index.ts
@@ -1,19 +1,15 @@
-import BaseComponent from './common/base';
 import logger from './common/logger';
 import { InputProps } from './common/entity';
 
-export default class ComponentDemo extends BaseComponent {
-  constructor(props) {
-    super(props)
-  }
+export default class ComponentDemo {
   /**
    * demo 实例
    * @param inputs
    * @returns
    */
   public async test(inputs: InputProps) {
-    logger.info('deploy test');
-    return { hello: 'hanxie' };
+    logger.debug(`input: ${JSON.stringify(inputs.props)}`);
+    logger.info('command test');
+    return { hello: 'world' };
   }
-
 }
